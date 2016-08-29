24377d4a98d75ca12c6f9e019e68a8fcc28bc77c
artem ovcharenko <akovcharenko"gmail.com>
2016-08-29 07:25:21 +0200

    start non stativ nav

--- js/component_navigation/navigation.jsx
+++ js/component_navigation/navigation.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-const mapStateToProps = state => {return {list: state.modifydList}};
+const mapStateToProps = state => {return {feedState: state.feedState}};
 
 class Navigation extends Component{
+    getUniqueLeagues(){
+        var feed = this.props.feedState;
+        var set = new Set();
+        feed.forEach(el => set.add(el.league));
+        return set;
+    }
+
     render(){
+        var leagues = this.getUniqueLeagues();
+        debugger;
         return <div className="league-navigation list-group col-sm-3 col-md-2">
+
+                    {leagues.map(this.eachItem)}
                     <a href="#" className="list-group-item">first league</a>
                     <a href="#" className="list-group-item">second league</a>
                     <a href="#" className="list-group-item">third league</a>
